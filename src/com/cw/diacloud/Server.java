746cee1bba8a2da17e563c5528d884f50c683ccf
cradbold <cwarmbold@sugarcrm.com>
2012-08-06 03:31:34 -0700

    working full cycle: rackspace

--- src/com/cw/diacloud/Server.java
+++ src/com/cw/diacloud/Server.java
@@ -1,32 +1,75 @@
 package com.cw.diacloud;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.StringWriter;
 import java.net.InetSocketAddress;
+
+import net.sf.json.JSONObject;
+import net.sf.json.JSONSerializer;
+
+import org.apache.commons.io.IOUtils;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
-public class Server implements HttpHandler {
+public class Server {
+	
+	public static final int PORT = 7777;
 
-	public void handle(HttpExchange t) throws IOException {
-		t.getRequestMethod();
-		InputStream is = t.getRequestBody();
-		//read(is); // .. read the request body
-		String response = "Hello World!";
-		t.sendResponseHeaders(200, response.length());
-		OutputStream os = t.getResponseBody();
-		os.write(response.getBytes());
-		os.close();
+	public static class ProvidersHandler implements HttpHandler {
+		public void handle(HttpExchange t) throws IOException {
+			try {
+				StringWriter writer = new StringWriter();
+				IOUtils.copy(t.getRequestBody(), writer, "UTF-8");
+				Server.logRequest(t, writer.toString());
+				String response = Parser.getProviders().toString();
+				t.sendResponseHeaders(200, response.length());
+				OutputStream os = t.getResponseBody();
+				os.write(response.getBytes());
+				os.close();
+				Server.logResponse(t, response);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 	}
-
+	
+	public static class InstancesHandler implements HttpHandler {
+		public void handle(HttpExchange t) throws IOException {
+			try {
+				StringWriter writer = new StringWriter();
+				IOUtils.copy(t.getRequestBody(), writer, "UTF-8");
+				Server.logRequest(t, writer.toString());
+				JSONObject json = (JSONObject) JSONSerializer.toJSON(writer.toString());
+				String response = Requester.getInstances(json.getJSONObject("provider").getString("name"));
+				t.sendResponseHeaders(200, response.length());
+				OutputStream os = t.getResponseBody();
+				os.write(response.getBytes());
+				os.close();
+				Server.logResponse(t, response);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	private static void logRequest(HttpExchange t, String body) throws IOException {
+		System.out.println("REQUEST: " + t.getRequestMethod() + " " + t.getLocalAddress() + t.getRequestURI());
+		System.out.println(" -BODY: " + body);
+	}
+	
+	private static void logResponse(HttpExchange t, String body) throws IOException {
+		System.out.println("RESPONSE: " + t.getResponseCode());
+		System.out.println(" -BODY: " + body);
+	}
+	
 	public static void main(String[] args) {
-		HttpServer server;
 		try {
-			server = HttpServer.create(new InetSocketAddress(7777), 0);
-			server.createContext("/helloworld", new Server());
+			HttpServer server = HttpServer.create(new InetSocketAddress(Server.PORT), 0);
+			server.createContext("/providers", new ProvidersHandler());
+			server.createContext("/instances", new InstancesHandler());
 			server.setExecutor(null);
 			server.start();
 		} catch (IOException e) {
--- src/com/cw/diacloud/test/Client.java
+++ src/com/cw/diacloud/test/Client.java
@@ -1,41 +1,72 @@
 package com.cw.diacloud.test;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.util.Iterator;
 
+import net.sf.json.JSONObject;
+import net.sf.json.JSONSerializer;
+
+import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
+
+import com.cw.diacloud.Server;
 
 
 public class Client {
 	
-	private static final String ENDPOINT = "http://localhost:7777/helloworld".toLowerCase();
+	private static final String ENDPOINT = "http://localhost:" + Server.PORT;
+	private static final String PROVIDERS_CONTEXT = "/providers";
+	private static final String INSTANCES_CONTEXT = "/instances";
 	
-    private static void getProviders() throws Exception {
+    private static String getProviders() throws Exception {
+    	String providers = null;
     	HttpClient httpclient = new DefaultHttpClient();
-        HttpGet httpget = new HttpGet(ENDPOINT);
+        HttpGet httpget = new HttpGet(ENDPOINT + PROVIDERS_CONTEXT);
 		HttpResponse response = httpclient.execute(httpget);
 		HttpEntity entity = response.getEntity();
 		if (entity != null) {
-			BufferedReader reader = new BufferedReader(new InputStreamReader(entity.getContent()));
-			String line = reader.readLine();
-			while (line != null) {
-				System.out.println(line);
-				line = reader.readLine();
-			}
+			providers = IOUtils.toString(entity.getContent());
 			httpget.abort();
 		}
 		httpclient.getConnectionManager().shutdown();
+		return providers;
+    }
+    
+    private static String getInstances(String providerName) throws Exception {
+    	String instances = null;
+    	HttpClient httpclient = new DefaultHttpClient();
+        HttpPost httppost = new HttpPost(ENDPOINT + INSTANCES_CONTEXT);
+        HttpEntity requestBody = new StringEntity("{\"provider\":{\"name\":\"rackspace\"}}");
+        httppost.setEntity(requestBody);
+		HttpResponse response = httpclient.execute(httppost);
+		HttpEntity entity = response.getEntity();
+		if (entity != null) {
+			instances = IOUtils.toString(entity.getContent());
+			httppost.abort();
+		}
+		httpclient.getConnectionManager().shutdown();
+		return instances;
     }
     
     	
+	@SuppressWarnings("unchecked")
 	public static void main(String[] args) {
 		try {
-			Client.getProviders();
+			String providers = Client.getProviders();
+//			System.out.println("providers:" + providers);
+			JSONObject json = (JSONObject) JSONSerializer.toJSON(providers);
+			Iterator<JSONObject> iterator = json.getJSONArray("providers").iterator();
+//			while (iterator.hasNext()) {
+				String providerName = iterator.next().getString("name");
+				System.out.println("Provider name:" + providerName);
+				String instances = Client.getInstances("rackspace");//providerName);
+				System.out.println(" -Instances:" + instances);
+//			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
